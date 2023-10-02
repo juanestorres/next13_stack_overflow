f93e9d9b54a47d7b42b1acef5777cb2fa9307c67
Juan Esteban Torres <juanes_1323@gotmail.com>
2023-10-01 19:41:12 -0500

    navbar and left sidebar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -9,21 +9,24 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState("dark");
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
-  //   useEffect(() => {
-  //     handleThemeChange();
-  //   }, [mode]);
-
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
       {children}
