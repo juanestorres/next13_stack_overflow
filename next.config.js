e7f4e85b6396fe4cffc4fe3451d36a46e6babb87
Juan Esteban Torres <juanes_1323@gotmail.com>
2024-01-08 16:50:04 -0500

    add user image to question card

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         serverActions: true,
       },
+      images: {
+        remotePatterns: [
+          {
+          protocol: 'https',
+          hostname: '*'
+          },
+          {
+          protocol: 'http',
+          hostname: '*'
+          },
+        ]
+      }
 }
 
 module.exports = nextConfig
